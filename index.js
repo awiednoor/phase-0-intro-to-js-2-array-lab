6342abec0a0efcce7287b3da9b8020676f301306
Noor Awied <nooorawied@gmail.com>
2022-04-23 20:56:08 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+  cats.push(name);
+  return cats;
+};
+
+const destructivelyPrependCat = (name) => {
+  cats.unshift(name);
+  return cats;
+};
+
+const destructivelyRemoveLastCat = (name) => {
+  cats.pop();
+  return cats;
+};
+const destructivelyRemoveFirstCat = (name) => {
+  cats.shift();
+  return cats;
+};
+
+const appendCat = (name) => {
+  let newarr = [...cats];
+  newarr.push(name);
+  return newarr;
+};
+
+const prependCat = (name) => {
+  let newarr = [...cats];
+  newarr.unshift(name);
+  return newarr;
+};
+
+const removeLastCat = () => {
+  let newarr = [...cats];
+  newarr.pop();
+  return newarr;
+};
+
+const removeFirstCat = () => {
+  let newarr = [...cats];
+  newarr.shift();
+  return newarr;
+};
+
+console.log(appendCat("Broom"));
+console.log(prependCat("Arnold"));
+console.log(removeLastCat());
+console.log(removeFirstCat());
